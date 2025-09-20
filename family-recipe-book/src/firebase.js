69a764e33a8e12638b2863d0cfd3dc41ec0d13ee
Hasnain Raza <razam@usc.edu>
2025-09-20 03:37:40 -0700

    fix: Move Firebase keys to .env file

--- family-recipe-book/src/firebase.js
+++ family-recipe-book/src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3Etq4c6xo3EFfTNF2GPKhfUkC8O-0tjU",
-  authDomain: "family-recipe-book-fc936.firebaseapp.com",
-  projectId: "family-recipe-book-fc936",
-  storageBucket: "family-recipe-book-fc936.firebasestorage.app",
-  messagingSenderId: "1088383543679",
-  appId: "1:1088383543679:web:8e31af4b8c3c018363d24a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
